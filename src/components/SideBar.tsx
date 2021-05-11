6cce92d5d076a9c8b72d19f044a8283b8c2e9c7a
Guilherme de Freitas Reis <guilhermereis9876@gmail.com>
2021-05-10 22:04:10 -0300

    ⭐ Desafio finalizado

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,38 @@
-export function SideBar() {
-  // Complete aqui
+import { GenreProps } from "../@types/Genre";
+import { Button } from "./Button";
+
+interface ISideBarProps {
+  selectedGenreId: any;
+  setSelectedGenreId: any;
+  genres: GenreProps[];
+}
+
+export function SideBar({
+  selectedGenreId,
+  setSelectedGenreId,
+  genres,
+}: ISideBarProps) {
+  function handleClickButton(id: number) {
+    setSelectedGenreId(id);
+  }
+
+  return (
+    <nav className="sidebar">
+      <span>
+        Watch<p>Me</p>
+      </span>
+
+      <div className="buttons-container">
+        {genres.map((genre) => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => handleClickButton(genre.id)}
+            selected={selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+    </nav>
+  );
 }
